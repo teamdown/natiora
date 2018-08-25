b04fc99e48a76ea62a968e41f9c98d4e5fdefcd2
nyandrianinamamy <nyandrianinamamy@gmail.com>
2018-08-25 11:06:57 +0300

    integrate blog , imgs

--- src/Blog.vue
+++ src/Blog.vue
@@ -1,3 +1,306 @@
 <template>
-    <p> Blog works</p>
+    <div id="app" class="main-container">
+        <div id="logo"><a href="http://html.blahlab.com/concis/index.html"><h1><img src="/static/assets/img/logo.png" alt=""></h1></a></div>
+        <div id="menu-wrapper">
+        <div id="menu-controller">
+            <div id="menu-icon">
+            <div id="menu-icon-stack">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <div id="menu-icon-close">
+                <span></span>
+                <span></span>
+            </div>
+            </div>
+            <div id="menu-caption">
+            <span id="menu-caption-open">
+                menu
+            </span>
+            <span id="menu-caption-close">
+                close
+            </span>
+            </div>
+        </div>
+        <div id="menu-content">
+            <nav id="main-menu">
+            <ul>
+                <li><a href="http://html.blahlab.com/concis/index.html">Home</a></li>
+                <li><a href="http://html.blahlab.com/concis/about.html">About</a></li>
+                <li><a href="http://html.blahlab.com/concis/services.html">Services</a></li>
+                <li>
+                <a href="http://html.blahlab.com/concis/cases.html">Cases</a>
+                <ul>
+                    <li><a href="http://html.blahlab.com/concis/case-study.html">Case study</a></li>
+                    <li><a href="http://html.blahlab.com/concis/case-study-2.html">Case study 2</a></li>
+                </ul>
+                </li>
+                <li><a href="http://html.blahlab.com/concis/blog.html">Blog</a></li>
+                <li><a href="http://html.blahlab.com/concis/contact.html">Contact</a></li>
+            </ul>
+            </nav>
+            <div id="contact-info">
+            <p>
+                120 K St, Second Floor <br>
+                Sacramento <br>
+                CA 95814
+            </p>
+            <p><a href="http://html.blahlab.com/concis/916.921.9278">916.921.9278</a></p>
+            <p><a href="mailto:business@concis.com" title="business email">business@concis.com</a></p>
+            </div>
+            <div id="socials">
+            <ul>
+                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+                <li><a href="#"><i class="fa fa-youtube"></i></a></li>
+            </ul>
+            </div>
+            <p id="copyright">
+            All rights reserved 2018 © concis
+            </p>
+        </div>
+        <div id="bg-primary" class=""></div>
+        <div id="bg-secondary"></div>
+        </div>
+        <div class="lines-wrapper">
+        <div class="horizontal-line"></div>
+        <div class="oblique-line"></div>
+        </div>
+        <div class="blue-triangle">
+        <img src="/static/assets/img/blue-triangle.png" alt="">
+        </div>
+        <div class="red-triangle">
+        <img src="/static/assets/img/red-triangle.png" alt="">
+        </div>
+        <div class="site-intro">
+        <h2>
+        Our blog
+        </h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+    Neque, repellat! Debitis fuga aut vitae dolore, consequuntur 
+    necessitatibus, quia voluptates architecto nesciunt doloremque. </p>
+        </div>
+        <div class="action-hint quick">Scroll or drag to explore.</div>
+        <div class="action-hint-for-touch">Swipe to explore.</div>
+
+        <!-- Swiper -->
+        <div class="swiper-container swiper-container-horizontal swiper-container-wp8-horizontal initialized" id="posts" style="cursor: grab;">
+        <div class="swiper-wrapper" style="transition-duration: 0ms; transform: translate3d(0px, 0px, 0px);">
+            <div class="swiper-slide sticky swiper-slide-active">
+            <img src="/static/assets/img/blog-1.jpg" alt="">
+            <div class="post-intro">
+                <h3><a href="http://html.blahlab.com/concis/single.html">You do not take a photograph, you make it.</a></h3>
+                <p class="info">
+                <span>
+                    By honryou
+                </span>
+                <span>20/12/2017</span>
+                <span>
+                    In
+                    <a href="#">Design</a>
+                </span>
+                <span>
+                    <a href="#">Comments 2</a>
+                </span>
+                </p>
+                <div class="two spacing"></div>
+                <p class="read-more-button">
+                <a href="http://html.blahlab.com/concis/single.html" class="button boxed small black">Read more</a>
+                </p>
+
+            </div>
+            </div>
+            <div class="swiper-slide swiper-slide-next">
+            <img src="/static/assets/img/blog-2.jpg" alt="">
+            <div class="post-intro">
+                <h3><a href="http://html.blahlab.com/concis/single.html">The real winners.</a></h3>
+                <p class="info">
+                <span>
+                    By honryou
+                </span>
+                <span>20/12/2017</span>
+                <span>
+                    In
+                    <a href="#">Design</a>
+                </span>
+                <span>
+                    <a href="#">Comments 2</a>
+                </span>
+                </p>
+                <div class="two spacing"></div>
+                <p class="read-more-button">
+                <a href="http://html.blahlab.com/concis/single.html" class="button boxed small black">Read more</a>
+                </p>
+            </div>
+            </div>
+            <div class="swiper-slide">
+            <img src="/static/assets/img/blog-3.jpg" alt="">
+            <div class="post-intro">
+                <h3><a href="http://html.blahlab.com/concis/single.html">Art is pattern informed by sensibility.</a></h3>
+                <p class="info">
+                <span>
+                    By honryou
+                </span>
+                <span>20/12/2017</span>
+                <span>
+                    In
+                    <a href="#">Design</a>
+                </span>
+                <span>
+                    <a href="#">Comments 2</a>
+                </span>
+                </p>
+                <div class="two spacing"></div>
+                <p class="read-more-button">
+                <a href="http://html.blahlab.com/concis/single.html" class="button boxed small black">Read more</a>
+                </p>
+            </div>
+            </div>
+            <div class="swiper-slide">
+            <img src="/static/assets/img/blog-4.jpg" alt="">
+            <div class="post-intro">
+                <h3><a href="http://html.blahlab.com/concis/single.html">Updating our identity.</a></h3>
+                <p class="info">
+                <span>
+                    By honryou
+                </span>
+                <span>20/12/2017</span>
+                <span>
+                    In
+                    <a href="#">Design</a>
+                </span>
+                <span>
+                    <a href="#">Comments 2</a>
+                </span>
+                </p>
+                <div class="two spacing"></div>
+                <p class="read-more-button">
+                <a href="http://html.blahlab.com/concis/single.html" class="button boxed small black">Read more</a>
+                </p>
+            </div>
+            </div>
+            <div class="swiper-slide">
+            <img src="/static/assets/img/blog-5.jpg" alt="">
+            <div class="post-intro">
+                <h3><a href="http://html.blahlab.com/concis/single.html">Art is pattern informed by sensibility.</a></h3>
+                <p class="info">
+                <span>
+                    By honryou
+                </span>
+                <span>20/12/2017</span>
+                <span>
+                    In
+                    <a href="#">Design</a>
+                </span>
+                <span>
+                    <a href="#">Comments 2</a>
+                </span>
+                </p>
+                <div class="two spacing"></div>
+                <p class="read-more-button">
+                <a href="http://html.blahlab.com/concis/single.html" class="button boxed small black">Read more</a>
+                </p>
+            </div>
+            </div>
+            <div class="swiper-slide">
+            <img src="/static/assets/img/blog-6.jpg" alt="">
+            <div class="post-intro">
+                <h3><a href="http://html.blahlab.com/concis/single.html">Updating our identity.</a></h3>
+                <p class="info">
+                <span>
+                    By honryou
+                </span>
+                <span>20/12/2017</span>
+                <span>
+                    In
+                    <a href="#">Design</a>
+                </span>
+                <span>
+                    <a href="#">Comments 2</a>
+                </span>
+                </p>
+                <div class="two spacing"></div>
+                <p class="read-more-button">
+                <a href="http://html.blahlab.com/concis/single.html" class="button boxed small black">Read more</a>
+                </p>
+            </div>
+            </div>
+            <div class="swiper-slide">
+            <img src="/static/assets/img/blog-7.jpg" alt="">
+            <div class="post-intro">
+                <h3><a href="http://html.blahlab.com/concis/single.html">The real winners.</a></h3>
+                <p class="info">
+                <span>
+                    By honryou
+                </span>
+                <span>20/12/2017</span>
+                <span>
+                    In
+                    <a href="#">Design</a>
+                </span>
+                <span>
+                    <a href="#">Comments 2</a>
+                </span>
+                </p>
+                <div class="two spacing"></div>
+                <p class="read-more-button">
+                <a href="http://html.blahlab.com/concis/single.html" class="button boxed small black">Read more</a>
+                </p>
+            </div>
+            </div>
+            <div class="swiper-slide">
+            <img src="/static/assets/img/blog-8.jpg" alt="">
+            <div class="post-intro">
+                <h3><a href="http://html.blahlab.com/concis/single.html">Art is pattern informed by sensibility.</a></h3>
+                <p class="info">
+                <span>
+                    By honryou
+                </span>
+                <span>20/12/2017</span>
+                <span>
+                    In
+                    <a href="#">Design</a>
+                </span>
+                <span>
+                    <a href="#">Comments 2</a>
+                </span>
+                </p>
+                <div class="two spacing"></div>
+                <p class="read-more-button">
+                <a href="http://html.blahlab.com/concis/single.html" class="button boxed small black">Read more</a>
+                </p>
+            </div>
+            </div>
+            <div class="swiper-slide">
+            <img src="/static/assets/img/blog-9.jpg" alt="">
+            <div class="post-intro">
+                <h3><a href="http://html.blahlab.com/concis/single.html">Updating our identity.</a></h3>
+                <p class="info">
+                <span>
+                    By honryou
+                </span>
+                <span>20/12/2017</span>
+                <span>
+                    In
+                    <a href="#">Design</a>
+                </span>
+                <span>
+                    <a href="#">Comments 2</a>
+                </span>
+                </p>
+                <div class="two spacing"></div>
+                <p class="read-more-button">
+                <a href="http://html.blahlab.com/concis/single.html" class="button boxed small black">Read more</a>
+                </p>
+            </div>
+            </div>
+            <div class="swiper-slide" id="load-more">
+            <p><a href="#">Load more...</a></p>
+            </div>
+        </div>
+        </div>
+    </div>
 </template>
